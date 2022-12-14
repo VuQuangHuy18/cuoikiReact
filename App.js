378d57f387701246ba8be86cef762b026f7e4901
VuQuangHuy18 <huy182001@gmail.com>
2022-12-14 08:06:56 +0700

    Cau 1

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { NavigationContainer } from '@react-navigation/native';
+
+import Screen1 from './Screen/Screen1';
+import Screen2 from './Screen/Screen2';
+import Screen3 from './Screen/Screen3';
+
+
+const Stack = createNativeStackNavigator();
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name='Screen1' component={Screen1} />
+        <Stack.Screen name='Screen2' component={Screen2} />
+        <Stack.Screen name='Screen3' component={Screen3} />
+
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ Screen/Screen1.js
@@ -0,0 +1,70 @@
+import { StyleSheet, Text, View, Animated, Image, TouchableOpacity,Button} from 'react-native'
+import React, { useEffect, useRef } from 'react'
+
+
+const Screen1 = ({navigation}) => {
+    const opasityMotion = useRef(new Animated.Value(0)).current;
+
+    useEffect(() => {
+        Animated.timing(
+          opasityMotion,
+          {
+            toValue: 1,
+            duration: 4000,
+            useNativeDriver: false
+          }
+        ).start();
+      }, []);
+
+
+
+  return (
+    <View style= {styles.container}>
+
+        <Animated.View style={styles.welcome}>
+            <Animated.Text style={{opacity:opasityMotion,fontSize: 20,fontWeight:'2000'}}>WELCOME TO FURISAS</Animated.Text>
+        </Animated.View>
+        <Text style={{flex:1,alignItems:'center',justifyContent:'center'}}>
+        A premium online store for women and their stylish choice
+        </Text>
+       <TouchableOpacity style={{justifyContent:'center',alignItems:'center'}}>
+         <View style={{backgroundColor:'black',width:100,borderRadius:20,height:40,justifyContent:'center',alignContent:'center',marginBottom:50}}>
+            <Text style={{color:'white',justifyContent:'center',alignContent:'center',marginLeft:10}}>Get Started</Text>
+         </View>
+       </TouchableOpacity>
+
+
+       <View style={{backgroundColor:'white',borderRadius:300}}>
+       <Image
+        style={{ width: 300, height: 300 }}
+        source={{
+          uri: 'https://res.cloudinary.com/dtwy0ch1a/image/upload/v1670978189/started_hat_1_rvvdqp.png',
+        }}
+      />
+       </View>
+
+       
+      
+    </View>
+  )
+}
+
+export default Screen1
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:'pink',
+        alignItems:'center',
+        justifyContent:'center'
+    },
+    welcome:
+    {
+        flex:1,
+        fontSize:300,
+        fontWeight:'700',
+        alignItems:'center',
+        marginTop:30
+
+    }
+})
